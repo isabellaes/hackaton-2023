dc4d7d90e18a9ce6ef1b0bec4521fe51cca8d55d
isabellaes <borjessonisabella@gmail.com>
2024-05-16 17:02:40 +0200

    updated routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
 import "./App.scss";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ActivityDetailsPage from "./pages/ActivityDetailPage.tsx";
+import ActivityPage from "./pages/ActivityPage.tsx";
+import HomePage from "./pages/HomePage.tsx";
 
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<HomePage />} />
+        <Route path="/activitys" element={<ActivityPage />} />
+        <Route path="/activity/:id" element={<ActivityDetailsPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
