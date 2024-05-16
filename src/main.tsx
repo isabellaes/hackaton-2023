44078f301e2e45c510f79d31bea9a8d77f63062b
JoohanK <94johkri1@skola.boras.se>
2024-05-16 19:29:57 +0200

    header added

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
-import "./index.scss"
-import { createBrowserRouter, RouterProvider } from "react-router-dom"
-import ActivityDetailsPage from "./pages/ActivityDetailPage.tsx"
-import ActivityPage from "./pages/ActivityPage.tsx"
-import HomePage from "./pages/HomePage.tsx"
-import "./style/style.scss"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.scss";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ActivityDetailsPage from "./pages/ActivityDetailPage.tsx";
+import ActivityPage from "./pages/ActivityPage.tsx";
+import HomePage from "./pages/HomePage.tsx";
+import "./style/style.scss";
+import Header from "./components/header/Header.tsx";
 
 const router = createBrowserRouter([
   {
@@ -16,10 +17,11 @@ const router = createBrowserRouter([
       { path: "activity/:id", element: <ActivityDetailsPage /> },
     ],
   },
-])
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
+    <Header />
     <RouterProvider router={router} />
   </React.StrictMode>
-)
+);
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,3 +1,5 @@
+import Header from "../components/header/Header";
+
 const HomePage = () => {
   return (
     <div>
