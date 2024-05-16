41754dace5e537c11de72e0d82eefb2de2987068
Lars Abelsson <georg483@msn.com>
2024-05-16 21:19:41 +0200

    Popup form

--- src/components/ActivityDateCalender/ActivityDateCalender.tsx
+++ src/components/ActivityDateCalender/ActivityDateCalender.tsx
@@ -2,18 +2,33 @@ import { useState } from "react"
 import "./ActivityDateCalender.scss"
 import Calender from "./Calender/Calender"
 import ActivityList from "../ActivityList/ActivityList"
+import Form from "../../Form/Form"
 const ActivityDateCalender = () => {
   const [date, setDate] = useState("")
   const setDateChosed = (date: string) => {
     setDate(date)
   }
+
+  const [showForm, setShowForm] = useState(false)
+  const setShowFormHandler = () => {
+    setShowForm(!showForm)
+  }
   return (
     <section className="activity-date-calender">
-      <Calender setDateChosed={setDateChosed} />
-      {date && (
-        <div>
-          <ActivityList />
-        </div>
+      <div>
+        <Calender setDateChosed={setDateChosed} />
+        {date && (
+          <div>
+            <ActivityList setShowFormHandler={setShowFormHandler} />
+          </div>
+        )}
+      </div>
+      {showForm && (
+        <Form
+          onClose={function (): void {
+            throw new Error("Function not implemented.")
+          }}
+        />
       )}
     </section>
   )
--- src/components/ActivityList/ActivityList.tsx
+++ src/components/ActivityList/ActivityList.tsx
@@ -1,39 +1,46 @@
 import "./ActivityList.scss"
 import yoga from "../../assets/yoga.jpg"
+import walk from "../../assets/walk-unsplash.jpg"
+import discgolf from "../../assets/discgolf-unsplash.jpg"
+import tabletennis from "../../assets/tabletennis-unsplash.jpg"
 
-const ActivityList = () => {
+const ActivityList = ({
+  setShowFormHandler,
+}: {
+  setShowFormHandler: () => void
+}) => {
   return (
     <section className="activity-list">
-      <div className="activity-list-card">
+      <div className="activity-list-card" onClick={setShowFormHandler}>
         <img src={yoga} alt="Aktivitetsbild" />
         <div className="activity-list-card-info">
           <h3>Yoga</h3>
           <p>Fristad</p>
+          <p>16.00 - 17.00</p>
+        </div>
+      </div>
+      <div className="activity-list-card" onClick={setShowFormHandler}>
+        <img src={walk} alt="Aktivitetsbild" />
+        <div className="activity-list-card-info">
+          <h3>Prommenad</h3>
+          <p>Borås</p>
           <p>10.00 - 11.00</p>
         </div>
       </div>
-      <div className="activity-list-card">
-        <img src="" alt="Aktivitetsbild" />
+      <div className="activity-list-card" onClick={setShowFormHandler}>
+        <img src={discgolf} alt="Aktivitetsbild" />
         <div className="activity-list-card-info">
-          <h3>Yoga</h3>
-          <p>Fristad</p>
-          <p>10.00 - 11.00</p>
+          <h3>Discgolf</h3>
+          <p>Dalsjöfors</p>
+          <p>17.00 - 19.00</p>
         </div>
       </div>
-      <div className="activity-list-card">
-        <img src="" alt="Aktivitetsbild" />
+      <div className="activity-list-card" onClick={setShowFormHandler}>
+        <img src={tabletennis} alt="Aktivitetsbild" />
         <div className="activity-list-card-info">
-          <h3>Yoga</h3>
-          <p>Fristad</p>
-          <p>10.00 - 11.00</p>
-        </div>
-      </div>
-      <div className="activity-list-card">
-        <img src="" alt="Aktivitetsbild" />
-        <div className="activity-list-card-info">
-          <h3>Yoga</h3>
-          <p>Fristad</p>
-          <p>10.00 - 11.00</p>
+          <h3>Bordtennis</h3>
+          <p>Byttorp</p>
+          <p>07.00 - 09.00</p>
         </div>
       </div>
     </section>
