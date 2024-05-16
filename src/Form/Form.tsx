508a21dbe8ea43f1de7941be6e4eca98a633c4b6
isabellaes <borjessonisabella@gmail.com>
2024-05-16 16:57:07 +0200

    added form details

--- src/Form/Form.tsx
+++ src/Form/Form.tsx
@@ -1,5 +1,16 @@
 const Form = () => {
-  return <form action=""></form>;
+  return (
+    <form className="form">
+      <h2>Anmäl dig till aktivitet</h2>
+      <label htmlFor="">Namn: </label>
+      <input type="text" />
+      <label htmlFor="">Email: </label>
+      <input type="email" />
+      <label htmlFor="">Ålder: </label>
+      <input type="number" name="" id="" />
+      <button>Skicka</button>
+    </form>
+  );
 };
 
 export default Form;
