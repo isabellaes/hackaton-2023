d041b7cb3d82090350636de2acaa63cd0bc79616
naathaliie <nathalielarsson@live.se>
2024-05-17 10:00:31 +0200

    activityPage

--- src/pages/ActivityPage/ActivityPage.tsx
+++ src/pages/ActivityPage/ActivityPage.tsx
@@ -1,11 +1,28 @@
 import { Link } from "react-router-dom";
+import "./ActivityPage.scss";
 
 const ActivityPage = () => {
   return (
-    <>
-      <h1>Activitys</h1>
-      <Link to={"/activity/6"}>Yoga</Link>
-    </>
+    <div className="ActivityPage">
+      <div className="activitys-box">
+        <h1>Aktiviteter</h1>
+        <Link className="activity-link" to={"/activity/6"}>
+          <h2>Basket</h2>
+        </Link>
+        <Link className="activity-link selected" to={"/activity/6"}>
+          <h2>Yoga</h2>
+        </Link>
+        <Link className="activity-link" to={"/activity/6"}>
+          <h2>Promenad</h2>
+        </Link>
+        <Link className="activity-link" to={"/activity/6"}>
+          <h2>Pingis</h2>
+        </Link>
+        <Link className="activity-link" to={"/activity/6"}>
+          <h2>Padel</h2>
+        </Link>
+      </div>
+    </div>
   );
 };
 
